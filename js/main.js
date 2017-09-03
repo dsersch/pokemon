70c850e71e04fc9c1998dd4bae60b8968e64c8d1
David Sersch <dsersch640@gmail.com>
2017-09-02 22:19:19 -0700

    added set name to player info function

--- js/main.js
+++ js/main.js
@@ -18,9 +18,10 @@ var smash = new Move('smash', 50, 5, 'rock', 'slategray')
 
 // pokemon constructor
 
-function Pokemon(name, HP, speed, type, move1, move2, move3, front, back){
+function Pokemon(name, HP, currentHP, speed, type, move1, move2, move3, front, back){
     this.name = name;
     this.HP = HP;
+    this.currentHP = currentHP
     this.speed = speed;
     this.type = type;
     this.moves = [move1, move2, move3]
@@ -33,19 +34,19 @@ function Pokemon(name, HP, speed, type, move1, move2, move3, front, back){
 
 Red = {
     player: 0,
-    pickacu: new Pokemon('picachu', 150, 9, 'electric', thunderbolt, shock, slap, 
+    pickacu: new Pokemon('PICACHU', 150, 150, 9, 'electric', thunderbolt, shock, slap, 
     'http://www.pokestadium.com/sprites/xy/pikachu-female.gif', 'http://www.pokestadium.com/sprites/xy/back/pikachu.gif'),
 
-    geodude: new Pokemon('geodude', 200, 3, 'rock', pound, rockThrow, smash,
+    geodude: new Pokemon('GEODUDE', 200, 200, 'rock', pound, rockThrow, smash,
     'http://www.pokestadium.com/sprites/xy/geodude.gif', 'http://www.pokestadium.com/sprites/xy/back/geodude.gif')
     
 }
 Blue = {
     player: 1,
-    geodude: new Pokemon('geodude', 200, 3, 'rock', pound, rockThrow, smash,
+    geodude: new Pokemon('GEODUDE', 200, 200, 3, 'rock', pound, rockThrow, smash,
     'http://www.pokestadium.com/sprites/xy/geodude.gif', 'http://www.pokestadium.com/sprites/xy/back/geodude.gif'),
 
-    pickacu: new Pokemon('picachu', 150, 9, 'electric', thunderbolt, shock, slap, 
+    pickacu: new Pokemon('PICACHU', 150, 150, 9, 'electric', thunderbolt, shock, slap, 
     'http://www.pokestadium.com/sprites/xy/pikachu-female.gif', 'http://www.pokestadium.com/sprites/xy/back/pikachu.gif')
 }
 
@@ -65,9 +66,15 @@ function setImage(trainer, poke) {
         $imgs[trainer.player].setAttribute('src', poke.front);
     }
 }
+
+function setName(trainer, poke) {
+    var $names = $('.name');
+    $($names[trainer.player]).text(poke.name)
+}
+
 function setHealt(trainer, poke) {
     var maxHealth = poke.HP;
-    var currentHealth = poke.HP;
+    var currentHealth = poke.currentHP;
     var $health = $('.health');
     $($health[trainer.player]).text('HP: ' + currentHealth + '/' + maxHealth)
 }
@@ -84,6 +91,7 @@ function setPlayerInfo(trainer, poke) {
     setImage(trainer, poke);
     setHealt(trainer, poke);
     setMoves(trainer, poke);
+    setName(trainer, poke);
 }
 
 setPlayerInfo(Red, Red.pickacu);
