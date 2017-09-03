7504930d4a967b852c4bff5c7bcc69a02b2fd28a
David Sersch <dsersch640@gmail.com>
2017-09-02 19:16:17 -0700

    html structure and move constructor

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 console.log("loaded...")
+// move constructor
+function Move(name, damage, pp, type, color){
+    this.name = name;
+    this.damage = damage;
+    this.pp = pp;
+    this.type = type;
+    this.color = color;
+}
+// test moves
+
+var thunderbolt = new Move('thunderbolt', 30, 25, 'electric', 'yellow');
+var slap = new Move('slap', 15, 40, 'nornmal', 'silver')
+var shock = new Move('shock', 50, 5, 'electric', 'yellow')
+
+// pokemon constructor
 
 function Pokemon(name, HP, speed, type, move1, move2, move3){
     this.name = name;
@@ -10,4 +25,6 @@ function Pokemon(name, HP, speed, type, move1, move2, move3){
     this.move3 = move3;
 }
 
-var pikachu = new Pokemon("Pikachu", 150, 9, "electric")
+// test pokemon object
+
+var pickacu = new Pokemon('picachu', 150, 9, 'electric', thunderbolt, shock, slap)
