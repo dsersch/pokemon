92ae2a1a8382d983e5acc99650195cd2705c6d96
David Sersch <dsersch640@gmail.com>
2017-09-02 19:01:25 -0700

    pokemon object constructor

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 console.log("loaded...")
 
+function Pokemon(name, HP, speed, type, move1, move2, move3){
+    this.name = name;
+    this.HP = HP;
+    this.speed = speed;
+    this.type = type;
+    this.move1 = move1;
+    this.move2 = move2;
+    this.move3 = move3;
+}
+
+var pikachu = new Pokemon("Pikachu", 150, 9, "electric")
