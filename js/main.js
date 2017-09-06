3df7d889c307ba6221c841fc1acf53747de2df7d
David Sersch <dsersch640@gmail.com>
2017-09-05 18:11:03 -0700

    type of attack and pokemon effect damage done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 console.log("loaded...")
 // last attack in display
 var lastAttack = '';
+
+// type constructor
+function Type(name, strong1, strong2, weak1, weak2){
+    this.name = name;
+    this.strong = [strong1, strong2];
+    this.weak = [weak1, weak2]
+}
+
+var electric = new Type('electric', 'water', 'flying', 'ground', 'none');
+var water = new Type('water', 'fire', 'rock', 'electric', 'grass')
+var fire = new Type('fire', 'grass', 'bug', 'water', 'ground')
+var grass = new Type('grass', 'water', 'ground', 'fire', 'flying')
+var ground = new Type('ground', 'electric', 'fire', 'water', 'grass')
+var rock = new Type('rock', 'fire', 'electric', 'water', 'grass')
+var flying = new Type('flying', 'grass', 'bug', 'electric', 'rock')
+var bug = new Type('bug', 'grass', 'psychic', 'fire', 'rock')
+
 // move constructor
 function Move(name, damage, pp, currentPP, accuracy, type, color){
     this.name = name;
@@ -31,22 +48,22 @@ Red = {
     player: 0,
     name: 'Red',
     koCount: 0,
-    pickacu: new Pokemon('PICACHU', 150, 150, 9, 'electric',
-    new Move('thunderbolt', 30, 25, 25, 8,'electric', 'yellow'),
-    new Move('shock', 50, 5, 5, 7,'electric', 'yellow'),
+    pickacu: new Pokemon('PICACHU', 150, 150, 9, electric,
+    new Move('thunderbolt', 30, 25, 25, 8, electric, 'yellow'),
+    new Move('shock', 50, 5, 5, 7, electric, 'yellow'),
     new Move('slap', 15, 40, 40, 9,'nornmal', 'silver'),
     'http://www.pokestadium.com/sprites/xy/pikachu-female.gif', 'http://www.pokestadium.com/sprites/xy/back/pikachu.gif'),
 
-    geodude: new Pokemon('GEODUDE', 200, 200, 3, 'rock',
+    geodude: new Pokemon('GEODUDE', 200, 200, 3, rock,
     new Move('pound', 15, 40, 40, 9, 'normal', 'silver'),
-    new Move('rock throw', 30, 25, 25, 8, 'rock', 'slategray'),
-    new Move('smash', 50, 5, 5, 7, 'rock', 'slategray'),
+    new Move('rock throw', 30, 25, 25, 8, rock, 'slategray'),
+    new Move('smash', 50, 5, 5, 7, rock, 'slategray'),
     'http://www.pokestadium.com/sprites/xy/geodude.gif', 'http://www.pokestadium.com/sprites/xy/back/geodude.gif'),
 
-    charizard: new Pokemon('CHARIZARD', 250, 250, 5, 'fire', 
+    charizard: new Pokemon('CHARIZARD', 250, 250, 5, fire, 
     new Move('punch', 15, 40, 40, 9,'normal', 'silver'),
-    new Move('ember', 30, 25, 25, 8, 'fire', 'tomato'),
-    new Move('fire blast', 50, 5, 5, 7, 'fire', 'tomato'),
+    new Move('ember', 30, 25, 25, 8, fire, 'tomato'),
+    new Move('fire blast', 50, 5, 5, 7, fire, 'tomato'),
     'http://www.pokestadium.com/sprites/xy/charizard.gif', 'http://www.pokestadium.com/sprites/xy/back/charizard.gif')
 }
 var redArray = [Red.pickacu, Red.geodude, Red.charizard]
@@ -54,22 +71,22 @@ Blue = {
     player: 1,
     name: 'Blue',
     koCount: 0,
-    geodude: new Pokemon('GEODUDE', 200, 200, 3, 'rock',
+    geodude: new Pokemon('GEODUDE', 200, 200, 3, rock,
     new Move('pound', 15, 40, 40, 9, 'normal', 'silver'),
-    new Move('rock throw', 30, 25, 25, 8, 'rock', 'slategray'),
-    new Move('smash', 50, 5, 5, 7, 'rock', 'slategray'),
+    new Move('rock throw', 30, 25, 25, 8, rock, 'slategray'),
+    new Move('smash', 50, 5, 5, 7, rock, 'slategray'),
     'http://www.pokestadium.com/sprites/xy/geodude.gif', 'http://www.pokestadium.com/sprites/xy/back/geodude.gif'),
 
-    pickacu: new Pokemon('PICACHU', 150, 150, 9, 'electric',
-    new Move('thunderbolt', 30, 25, 25, 8, 'electric', 'yellow'),
-    new Move('shock', 50, 5, 5, 7, 'electric', 'yellow'),
+    pickacu: new Pokemon('PICACHU', 150, 150, 9, electric,
+    new Move('thunderbolt', 30, 25, 25, 8, electric, 'yellow'),
+    new Move('shock', 50, 5, 5, 7, electric, 'yellow'),
     new Move('slap', 15, 40, 40, 9, 'nornmal', 'silver'), 
     'http://www.pokestadium.com/sprites/xy/pikachu-female.gif', 'http://www.pokestadium.com/sprites/xy/back/pikachu.gif'),
 
-    blastoise: new Pokemon('BLASTOISE', 300, 300, 4, 'water',
+    blastoise: new Pokemon('BLASTOISE', 300, 300, 4, water,
     new Move('drop kick', 15, 40, 40, 9, 'normal', 'silver'),
-    new Move('water gun', 30, 25, 25, 8, 'water', 'dodgerblue'),
-    new Move('hydro pump', 50, 5, 5, 7, 'water', 'dodgerblue'),
+    new Move('water gun', 30, 25, 25, 8, water, 'dodgerblue'),
+    new Move('hydro pump', 50, 5, 5, 7, water, 'dodgerblue'),
     'http://www.pokestadium.com/sprites/xy/blastoise.gif', 'http://www.pokestadium.com/sprites/xy/back/blastoise.gif')
 
 }
@@ -217,13 +234,6 @@ $('.ball').hover(function() {
 }, function() {
     display(lastAttack);
 })
-// $('body').on('mouseenter', '.ball', function() {
-//     $(this).html('<img src="' + this.pokemon.front + '">')
-// })
-// $('body').on('mouseleave', '.ball', function() {  
-//         $(this).html('<img src="' + this.pokemon.back + '">')
-// })
-
 
 // remove ko'd pokemon
 
@@ -258,21 +268,58 @@ $('.attack').hover(function() {
     display(lastAttack)
 })
 
+// type check
+
+
 // attack function
 
 $('body').on('click', '.attack', function() {
-    // var acc = Math.floor(Math.random() *10)
-    // console.log(acc)
     if (Math.floor(Math.random() *10) < this.move.accuracy) {
         if (this.move.currentPP === 0) {
             display("Out of Power Points. Select another attack.")
         } else {
-            enemyPokemon.currentHP -= this.move.damage;
+            // set damage based on type
+            var pokeBonus = 0;
+            if (this.move.type === currentPokemon.type) {
+                console.log('move matches');
+                pokeBonus = Math.floor(this.move.damage * .25);
+                console.log(pokeBonus)
+            }
+            var moveBonus = 0
+            if (this.move.type.name === enemyPokemon.type.weak[0] || this.move.type === enemyPokemon.type.weak[1]) {
+                console.log('move strong against type');
+                display("It's super effective")
+                moveBonus = Math.floor(this.move.damage * .5);
+                console.log(moveBonus)
+            }
+            var typeReduction = 1;
+            if(this.move.type.name === enemyPokemon.type.strong[0] || this.move.type.name === enemyPokemon.type.strong[1]) {
+                typeReduction = .1
+            }
+            // damage the enemy
+            
+            var totalDamage = Math.floor((this.move.damage + pokeBonus + moveBonus) * typeReduction);
+            enemyPokemon.currentHP -= totalDamage;
             this.move.currentPP -= 1;
             $(this).children().text("PP: " + this.move.currentPP + '/' + this.move.pp)
+            
             //damage alert
-            display(currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + this.move.damage + ' damage.')
-            lastAttack = currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + this.move.damage + ' damage.'
+
+            if (moveBonus > 0) {
+                display(currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + totalDamage + ' damage.' + '<br>' +
+                            "It's super effective");
+                lastAttack = currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + totalDamage + ' damage.' + '<br>' +
+                "It's super effective"
+            } else if (typeReduction === .1) {
+                display(currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + totalDamage + ' damage.' + '<br>' +
+                            "It's not very effective");
+                lastAttack = currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + totalDamage + ' damage.' + '<br>' +
+                "It's not very effective";
+            } else {
+                display(currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + totalDamage + ' damage.');
+                lastAttack = currentPokemon.name + ' hit ' + enemyPokemon.name + ' for ' + totalDamage + ' damage.';
+            }
+            
             setHealth(enemy, enemyPokemon);
             // ko check
             if (enemyPokemon.currentHP <= 0) {
@@ -288,7 +335,8 @@ $('body').on('click', '.attack', function() {
             }
         }
     } else {
-        display("The attack missed!");
+        display(currentPokemon.name + "'s attack missed!");
+        lastAttack = currentPokemon.name + "'s attack missed!"
         this.move.currentPP -= 1;
         $(this).children().text("PP: " + this.move.currentPP + '/' + this.move.pp)
         switchTurns();
